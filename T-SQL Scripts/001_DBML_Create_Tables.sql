c2a1d1ea60298423019771e9a57a6c155cce224d
Omar Trabelsi <omar.trabelsi.work@outlook.fr>
2021-05-17 01:34:57 +0200

    adding init database scripts

---
+++ T-SQL Scripts/001_DBML_Create_Tables.sql
@@ -0,0 +1,26 @@
+
+if exists ( select 1 from sysobjects where id = OBJECT_ID('T_Vote') and type = 'U')
+drop table T_Vote
+Go
+
+
+if exists ( select 1 from sysobjects where id = OBJECT_ID('T_CatPicture') and type = 'U')
+drop table T_Cat
+Go
+
+
+CREATE TABLE [T_CatPicture] (
+    [Id] INT NOT NULL IDENTITY,
+    [Url] nvarchar(max) NOT NULL,
+    CONSTRAINT [PK_Cart] PRIMARY KEY ([Id]),
+);
+GO
+
+CREATE TABLE [T_Vote] (
+    [VoteId] int NOT NULL IDENTITY,
+    [CatId] int NOT NULL,
+    [CreationDate] datetime NULL
+    CONSTRAINT [PK_Vote] PRIMARY KEY ([VoteId]),
+    CONSTRAINT [FK_Vote_WinCat_CatId] FOREIGN KEY ([CatId]) REFERENCES [T_CatPicture] ([Id]),
+);
+GO
